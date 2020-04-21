2acfd79b91adfb362abb21219089f0bff94f49c6
liuwenhua <liuwenhua@lanjignren.com>
2020-04-21 19:46:30 +0800

    A first

--- Example/MKHotfix/MKViewController.m
+++ Example/MKHotfix/MKViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "MKViewController.h"
+#import "MKHotfix.h"
+#import "TestClass.h"
 
 @interface MKViewController ()
 
@@ -14,16 +16,90 @@
 
 @implementation MKViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self initHotfix];
+    [self testHotfix];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)initHotfix {
+    [[MKHotfix shared] fixWithFileName:@"js/hotfix"];
+}
+
+- (void)divider {
+    NSLog(@"=========");
+}
+
+- (void)testHotfix {
+    TestClass *t1 = [TestClass new];
+    {
+        NSString *res = [t1 testStringInOut:@"入参"];
+        NSLog(@"OC[testStringInOut] = %@", res);
+    }
+    [self divider];
+    {
+        int res = [t1 testIntInOut:1];
+        NSLog(@"OC[testIntInOut] = %d", res);
+    }
+    [self divider];
+    {
+        NSInteger res = [t1 testNSIntegerInOut:1];
+        NSLog(@"OC[testNSIntegerInOut] = %ld", res);
+    }
+    [self divider];
+    {
+        BOOL res = [t1 testBoolInOut:YES];
+        NSLog(@"OC[testBoolInOut] = %@", res ? @"YES" : @"NO");
+    }
+    [self divider];
+    {
+        char p = 'a';
+        char res = [t1 testCharInOut:p];
+        NSLog(@"OC[testCharInOut] = %c", res);
+    }
+    [self divider];
+    {
+        long long p = 1;
+        long long res = [t1 testLongLongInOut:p];
+        NSLog(@"OC[testLongLongInOut] = %lli", res);
+    }
+    [self divider];
+    {
+        long  p = 1;
+        long res = [t1 testLongInOut:p];
+        NSLog(@"OC[testLongInOut] = %li", res);
+    }
+    [self divider];
+    {
+        short  p = 1;
+        short res = [t1 testShortInOut:p];
+        NSLog(@"OC[testShortInOut] = %hi", res);
+    }
+    [self divider];
+    {
+        double p = 1;
+        double res = [t1 testDoubleInOut:p];
+        NSLog(@"OC[testDoubleInOut] = %lf", res);
+    }
+    [self divider];
+    {
+        Class clazz = TestClass.class;
+        Class res = [t1 testClassInOut:clazz];
+        NSLog(@"OC[testClassInOut] = %@", res);
+    }
+    [self divider];
+    {
+        NSDictionary *res = [t1 testNSDictionaryInOut:@{@"d": @1}];
+        NSLog(@"OC[testNSDictionaryInOut] = %@", res);
+    }
+    [self divider];
+    {
+        NSArray *res = [t1 testArrayInOut:@[@"1"]];
+        NSLog(@"OC[testArrayInOut] = %@", res);
+    }
+
+    NSLog(@"END");
+
 }
 
 @end
--- README.md
+++ README.md
@@ -1,29 +1,56 @@
 # MKHotfix
 
-[![CI Status](https://img.shields.io/travis/hua1017059@163.com/MKHotfix.svg?style=flat)](https://travis-ci.org/hua1017059@163.com/MKHotfix)
-[![Version](https://img.shields.io/cocoapods/v/MKHotfix.svg?style=flat)](https://cocoapods.org/pods/MKHotfix)
-[![License](https://img.shields.io/cocoapods/l/MKHotfix.svg?style=flat)](https://cocoapods.org/pods/MKHotfix)
-[![Platform](https://img.shields.io/cocoapods/p/MKHotfix.svg?style=flat)](https://cocoapods.org/pods/MKHotfix)
+![Version](https://img.shields.io/badge/version-1.0-blue.svg)
 
-## Example
-
-To run the example project, clone the repo, and run `pod install` from the Example directory first.
-
-## Requirements
+轻量JS热修工具
 
 ## Installation
-
-MKHotfix is available through [CocoaPods](https://cocoapods.org). To install
-it, simply add the following line to your Podfile:
 
 ```ruby
 pod 'MKHotfix'
 ```
 
-## Author
+## Example
 
-hua1017059@163.com, liuwenhua@lanjignren.com
+#### 热修功能
 
-## License
+* 类函数 执行前/后添加切面函数和替换类函数
+* 实例 函数执行前/后添加切面函数和替换实例函数
+* 修改 类/实例 函数的入参和返回值
+* 执行 类/实例 函数
 
-MKHotfix is available under the MIT license. See the LICENSE file for more info.
+#### 能力简介
+
+```
+fixInstanceMethodReplace("TestClass", "testIntInOut:", function(instance, originInvocation, originArguments) {
+    /**
+     * instance: 函数执行的实例
+     * originInvocation: 原函数
+     * originArguments: 原参数
+     */
+    /**
+     * 可在此处进行修复逻辑的处理, originArguments[index], 取出原参数
+     */
+    console.log("testStringInOut args = " + originArguments);
+    /// setInvocationParameter(originInvocation, $value, $index), 可更新函数入参
+    setInvocationParameter(originInvocation, 4, 0);
+    /// 更改入参后, 执行原函数, 拿到原函数返回结果
+    var originRes = runInvocation(originInvocation);
+    console.log("testStringInOut originRes = " + originRes);
+    return originRes; /// 将结果返回到业务调用处
+});
+```
+
+其他能力参见Demo
+
+```
+.
+├── MKHotfix
+│   ├── Demo
+│   │   ├── TestClass.h : 样例修复的目标类
+│   │   └── TestClass.m
+│   ├── MKViewController.h : 业务执行类
+│   ├── MKViewController.m
+└────── js
+        └── hotfix.js : 修复脚本
+```
